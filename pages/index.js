2b1638321bd4bff14fc69eea5ccb4845e4f972cb
snegurova <snegurova@gmail.com>
2021-08-05 13:07:37 +0300

    feat: generate password with options set length, include numbers

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import generator from  'generate-password';
 
 export default function Home() {
-  const isGenerated = true;
+  
+  const [isGenerated, setIsGenerated] = useState(false);
+  const [passwordValue, setPasswordValue] = useState('');
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const minLength = 6;
+  const maxLength = 12;
+  const [passwordLength, setPasswordLength] = useState(minLength);
+  
+  const handleGeneratePassword = () => {
+    const password = generator.generate({
+      length: passwordLength,
+      numbers: includeNumbers
+    });
+    setIsGenerated(true);
+    setPasswordValue(password);
+  }
+  
+  const handlePasswordLength = ({target}) => {
+    setPasswordLength(target.value);
+  }
+  
+  const handleIncludeNumbers = ({target}) => {
+    setIncludeNumbers(target.checked);
+  }
+  
+  const lengthOptions = [];
+  for (let i = minLength; i <= maxLength; i++) {
+    lengthOptions.push(<option value={i} key={i}>{i}</option>);
+  }
   
   return (
     <div className={styles.container}>
@@ -16,22 +46,21 @@ export default function Home() {
           Password Generator
         </h1>
         <div className={styles.controls}>
-          <select className={styles.mr15} name="passwordLength" id="passwordLength">
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
+          <select
+            className={styles.mr15}
+            name="passwordLength"
+            id="passwordLength"
+            onChange={handlePasswordLength}
+          >
+            {lengthOptions}
           </select>
   
-          <input type="checkbox" name="withNumbers" id="withNumbers"/>
+          <input type="checkbox" name="withNumbers" id="withNumbers" onChange={handleIncludeNumbers}/>
           <label className={styles.mr15} htmlFor="withNumbers">Include numbers</label>
-          <button>Generate password</button>
+          <button onClick={handleGeneratePassword}>Generate password</button>
         </div>
         <div style={ isGenerated ? { display:'block'} : {display : 'none'} }>
-          <input type="text" readOnly value="pass"/>
+          <input type="text" readOnly value={passwordValue}/>
           <button>Copy</button>
         </div>
       </main>
